570e8aad990f2213de34f07332440602d6874b0f
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2026-06-08 20:59:05 +0530

    fix: improve subscription logic and enhance error handling

--- backend/youtube-backend/src/controller/subscription.controller.js
+++ backend/youtube-backend/src/controller/subscription.controller.js
@@ -1,129 +1,136 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+  const { channelId } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(channelId)){
-        throw new ApiError(400, "Invalid ID format");
-    }
+  if (!mongoose.Types.ObjectId.isValid(channelId)) {
+    throw new ApiError(400, "Invalid ID format");
+  }
 
-    const alreadysubscribed = await Subscription.findOneAndDelete({subscriber:req.user._id ,channel:channelId})
-    
-    if(!alreadysubscribed)
-    {const subscription = await Subscription.create({
-        subscriber:req.user._id,
-        channel:channelId
-    })}
-    
+  const alreadysubscribed = await Subscription.findOneAndDelete({
+    subscriber: req.user._id,
+    channel: channelId,
+  });
 
-    return res.status(200).json(new ApiResponse(200,{},"added to subscription successfully"))
-})
+  if (!alreadysubscribed) {
+    await Subscription.create({
+      subscriber: req.user._id,
+      channel: channelId,
+    });
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        {},
+        alreadysubscribed
+          ? "unsubscribed successfully"
+          : "subscribed successfully",
+      ),
+    );
+});
 
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    
+  const { channelId } = req.params;
 
-    const subscribers = await Subscription.aggregate([
-        {
-            $match:{
-                 channel: new mongoose.Types.ObjectId(req.user._id)
-            }
-        },
-        {
-            $lookup:{
-                from:"users",
-                localField:"subscriber",
-                foreignField:"_id",
-                as:"subscriber",
-                pipeline:[
-                     { $project: {fullName: 1,username: 1,avatar: 1}}
-                ]
-            }
-            
-        },
-        {
-           $addFields:{
-            subscriber:{
-            $first: "$subscriber"
-               }
-           }
-        },
+  if (!mongoose.Types.ObjectId.isValid(channelId)) {
+    throw new ApiError(400, "Invalid channel ID");
+  }
 
-        {
-          $facet: {
-          subscribers: [{ $replaceRoot: { newRoot: "$subscriber" } }],
-          totalCount: [{ $count: "count" }]
-        }
-        },
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "subscriber",
+        pipeline: [{ $project: { fullName: 1, username: 1, avatar: 1 } }],
+      },
+    },
+    {
+      $addFields: {
+        subscriber: { $first: "$subscriber" },
+      },
+    },
+    {
+      $facet: {
+        subscribers: [{ $replaceRoot: { newRoot: "$subscriber" } }],
+        totalCount: [{ $count: "count" }],
+      },
+    },
+  ]);
 
-  
-    ])
+  const data = subscribers[0]?.subscribers || [];
+  const totalCount = subscribers[0]?.totalCount[0]?.count || 0;
 
-    const response = {
-            data: subscribers[0].subscribers,
-            totalCount: subscribers[0].totalCount[0]?.count || 0
-                       }
-
-    res.status(200).json(
-        new ApiResponse(200,response,"subscribers fetched successfully")
-    )
-})
-
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { data, totalCount },
+        "subscribers fetched successfully",
+      ),
+    );
+});
 
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-     const channels = await Subscription.aggregate([
-        {
-            $match:{
-                 subscriber: new mongoose.Types.ObjectId(req.user._id)
-            }
+  if (!mongoose.Types.ObjectId.isValid(req.user._id)) {
+    throw new ApiError(400, "Invalid user ID");
+  }
+  const channels = await Subscription.aggregate([
+    {
+      $match: {
+        subscriber: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "channel",
+        pipeline: [{ $project: { fullName: 1, username: 1, avatar: 1 } }],
+      },
+    },
+    {
+      $addFields: {
+        channel: {
+          $first: "$channel",
         },
-        {
-            $lookup:{
-                from:"users",
-                localField:"channel",
-                foreignField:"_id",
-                as:"channel",
-                pipeline:[
-                     { $project: {fullName: 1,username: 1,avatar: 1}}
-                ]
-            }
-            
-        },
-        {
-           $addFields:{
-            channel:{
-            $first: "$channel"
-               }
-           }
-        },
+      },
+    },
 
-        {
-          $facet: {
-          channels: [{ $replaceRoot: { newRoot: "$channel" } }],
-           totalCount: [{ $count: "count" }]
-        }
-        }
-  
-    ])
+    {
+      $facet: {
+        channels: [{ $replaceRoot: { newRoot: "$channel" } }],
+        totalCount: [{ $count: "count" }],
+      },
+    },
+  ]);
+  const data = channels[0]?.channels || [];
+  const totalCount = channels[0]?.totalCount[0]?.count || 0;
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { data, totalCount },
+        "subscribers fetched successfully",
+      ),
+    );
+});
 
-    const response = {
-        data: channels[0].channels,
-        totalCount: channels[0].totalCount[0]?.count || 0
-          }
-
-    res.status(200).json(
-        new ApiResponse(200,response,"channels  fetched successfully")
-    )
-})
-
-
-
-export{ 
-    toggleSubscription,
-    getUserChannelSubscribers,
-    getSubscribedChannels
-}
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
